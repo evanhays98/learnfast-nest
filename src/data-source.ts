2458f841067e78ecced86a5efb77d6321222d922
evanhays98 <evan.hays@epita.fr>
2023-10-14 15:36:22 +0200

    remove env file

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'learnrepetition',
-  password: 'learnrepetition',
-  database: 'learnrepetition',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: ['src/**/*Entity.ts'],
   subscribers: [],
   migrations: ['src/migrations/*.ts'],
