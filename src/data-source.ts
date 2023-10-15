335601f7808e6a442da0dfdb96fe4e7b66a6647a
evanFix <evan.hays98@gmail.com>
2023-10-15 12:26:35 +0200

    safe version

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,12 +1,14 @@
 import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
 
+dotenv.config(); // Load environment variables from .env file
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'evandata',
-  password: 'evandata',
-  database: 'evandata',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: ['src/**/*Entity.ts'],
   subscribers: [],
   migrations: ['src/migrations/*.ts'],
